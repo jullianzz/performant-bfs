05e31aedbb03df5d6da329109e6627ef785308ba
Zeng <zjulia@bu.edu>
2022-04-27 19:06:47 -0400

    ideas for bfs implementation

--- serial.c
+++ serial.c
@@ -16,4 +16,19 @@
 
 // TODO : serial code
 
+// plan for bfs serial implementation
+// graphs are represented by matrices
+// I need to write a bfs algorithm that works on adjacency matrices
+
+
+// algorithm
+// data structures: adjacency matrix (row=vertex col=neighbor), visited node list,
+//		    pointer to current vertex of interest in visited list, 
+// idea: pick a vertex at random from the neighbor list
+// traverse the adjacency matrix for the first neighbor in the row 
+// add look-up the neighbor in the visited list: if found, move on to next neighbor, if not,
+// add to visited list
+// keep iterating through the neighbors until all are found for the vertex
+// move on to next vertex by consulting the visited list pointer
+// do this until reach the end of the visited list
 
